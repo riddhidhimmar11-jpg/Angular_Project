7099c98725d7c137f8ec01ad44b071918bd62db5
riddhi <riddhidhimmar11@gmail.com>
2025-12-08 18:15:07 +0530

    h1 tag enter

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,8 +4,14 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  //templateUrl: './app.html',
+  //styleUrl: './app.css'
+  template:
+  `<h1>Good morning</h1>`,
+  styles:'h1{color:red;}'
+  
+  
+
 })
 export class App {
   protected readonly title = signal('Agproject');
