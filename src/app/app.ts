0e96a296f89537c736c684e296b001cabe5ce899
riddhi <riddhidhimmar11@gmail.com>
2025-12-10 15:34:53 +0530

    made footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Home } from './home/home';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header,Home],
+  imports: [RouterOutlet,Header,Home,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
   //template:
