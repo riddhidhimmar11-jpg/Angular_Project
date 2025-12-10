ed879dd90a584a9f897a644ed4d45e87f4427262
riddhi <riddhidhimmar11@gmail.com>
2025-12-10 11:54:29 +0530

    create header and home page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  //templateUrl: './app.html',
-  //styleUrl: './app.css'
-  template:
-  `<h1>Good morning</h1>`,
-  styles:'h1{color:red;}'
+  imports: [RouterOutlet,Header,Home],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+  //template:
+  //`<h1>Good morning</h1>`,
+  //styles:'h1{color:red;}'
   
   
 
